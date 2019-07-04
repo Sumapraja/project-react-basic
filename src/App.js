fb6c2674e5c9066b7185e892a6d414954fd76335
Sumapraja <irfansumapraja@gmail.com>
2019-07-04 09:20:17 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './illuminati.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This site contains viruses. Leave at once!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.illuminatiofficial.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Last Warning!
         </a>
       </header>
     </div>
